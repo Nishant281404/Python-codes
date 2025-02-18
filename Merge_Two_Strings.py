9e0c81c146e4feb22635a4414515269402473e4e
Nishant Tyagi <87650950+Nishant281404@users.noreply.github.com>
2025-02-18 06:08:06 +0530

    Update Merge_Two_Strings.py

--- Merge_Two_Strings.py
+++ Merge_Two_Strings.py
@@ -12,3 +12,16 @@ class Solution:
         while j != len(S2):
             s += S2[j]
         return s    # can generate TLE
+################################### APPROACH 2 ---> also generating TLE ###########################       
+        
+class Solution:
+    def merge(self, S1, S2):
+        res = []
+        i,j = 0,0
+        while i<len(S1) and j<len(S2):
+            res.append(S1[i])
+            res.append(S2[j])
+        res.append(S1[i])    
+        res.append(S2[j])
+        return "".join(res)
+        # code here
